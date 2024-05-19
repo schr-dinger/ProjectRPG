c996c2d6c2f1dbf36f8ada9d0857dbfd7f5d4659
schr-dinger <janghyunsik@naver.com>
2024-05-20 00:02:54 +0900

    10.Behavior Test
    Making Behavior Tree and linking them to monsters
    Made AIController and BB.
    Monsters Perception gets them a Target to follow

---
+++ 10SteamSessions_BehaviorTest/Source/SteamSessions/C_AIController.h
@@ -0,0 +1,74 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "C_TopDownController.h"
+#include "C_TopDownCharacter.h"
+#include "C_AIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEAMSESSIONS_API AC_AIController : public AAIController
+{
+	GENERATED_BODY()
+private:
+	UPROPERTY(EditAnywhere)
+	bool bDrawDebug = true;
+
+	UPROPERTY(EditAnywhere)
+	float AttackRange = 150;
+
+	UPROPERTY(EditAnywhere)
+	float AdjustCircleHeight = 50;
+
+	UPROPERTY(VisibleDefaultsOnly)
+	class UAIPerceptionComponent* Perception;
+
+	UPROPERTY(VisibleDefaultsOnly)
+	class UC_AIBehaviorComponent* Behavior;
+
+public:
+	AC_AIController();
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void OnPossess(APawn* InPawn) override;
+	virtual void OnUnPossess() override;
+
+	class UC_AIBehaviorComponent* GetBehavior() { return Behavior; }
+	float GetAttackRange() { return AttackRange; }
+	float GetSightRange();
+
+	TArray<AActor*> PlayerList;
+
+	//Aggro Testing
+	TMap<AC_TopDownCharacter*, float> AggroTable;
+
+	// 어그로 값을 증가시키는 함수
+	void IncreaseAggro(AC_TopDownCharacter* Player, float Amount);
+
+	// 가장 높은 어그로 값을 가진 타겟을 반환하는 함수
+	AC_TopDownCharacter* GetHighestAggroTarget();
+
+	// RPC 함수
+	UFUNCTION(Server, Reliable)
+	void ServerIncreaseAggro(AC_TopDownCharacter* Player, float Amount);
+
+	void UpdateTarget();
+
+private:
+	UFUNCTION()
+	void OnPerceptionUpdated(const TArray<AActor*>& UpdateActors);
+
+private:
+	class AC_BasicMonster* ControlledPawn;
+	class UAISenseConfig_Sight* Sight;
+
+	bool test = false;
+	
+};
---
+++ 10SteamSessions_BehaviorTest/Source/SteamSessions/C_TopDownCharacter.h
@@ -0,0 +1,116 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "CUW_CharacterOverHead.h"
+#include "C_TopDownCharacter.generated.h"
+
+UCLASS(Blueprintable)
+class STEAMSESSIONS_API AC_TopDownCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Sets default values for this character's properties
+	AC_TopDownCharacter();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	/** Returns TopDownCameraComponent subobject **/
+	FORCEINLINE class UCameraComponent* GetTopDownCameraComponent() const { return TopDownCameraComponent; }
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+
+	// Called to bind functionality to input
+	//virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION(Server, WithValidation, Reliable)
+	void SerRPCRoll();
+
+	UFUNCTION(NetMulticast, WithValidation, Reliable)
+	void MulRPCRoll();
+
+	UFUNCTION(NetMulticast,Reliable)
+	void SetCharacterName(const FString& name);
+
+
+	UFUNCTION(Server, Reliable, BlueprintCallable)
+	void TakeDamage(float damageamount);
+
+
+	UFUNCTION(NetMulticast, Reliable , BlueprintCallable)
+	void UpdateHpPercent(float curHp, float maxHp);
+
+
+
+
+	//skills
+
+	UFUNCTION(Server, Reliable)
+	void SerRPCAttack(UAnimMontage* AttackMon);
+
+	UFUNCTION(NetMulticast, Reliable)
+	void MulRPCAttack(UAnimMontage* AttackMon);
+
+
+
+
+
+protected:
+
+public:
+	UPROPERTY(EditAnywhere)
+	class UAnimMontage* RollMotion;
+
+	UPROPERTY(EditAnywhere)
+	class UAnimMontage* MeleeMotion;
+
+	UPROPERTY(EditAnywhere)
+	class UAnimMontage* QMotion;
+
+	UPROPERTY(EditAnywhere)
+	class UAnimMontage* WMotion;
+
+	UPROPERTY(EditAnywhere)
+	class UAnimMontage* EMotion;
+
+private:
+
+	/** Top down camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* TopDownCameraComponent;
+
+	/** Camera boom positioning the camera above the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+
+private:
+
+	//Over Head UI
+	//UPROPERTY()
+	//TSubclassOf<UCUW_CharacterOverHead> OverHeadUIClass;
+	//UCUW_CharacterOverHead* OverHeadUI;
+
+	UPROPERTY(EditAnywhere)
+	class UWidgetComponent* OverHeadUI;
+
+public:
+
+	UPROPERTY(VisibleAnywhere)
+	FString CharacterName ;
+
+	UPROPERTY(VisibleAnywhere,BlueprintReadWrite)
+	float CharacterHp = 100.0f;
+
+
+};
