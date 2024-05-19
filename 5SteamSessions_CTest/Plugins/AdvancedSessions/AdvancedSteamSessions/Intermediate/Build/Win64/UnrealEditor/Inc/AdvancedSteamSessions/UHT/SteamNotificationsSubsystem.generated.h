3a4ad0ea1dc40b10f7238bcaf3c5f3cf91607370
schr-dinger <janghyunsik@naver.com>
2024-05-19 23:16:29 +0900

    5.Decided to make the Project in C++
    Converting everything I made to C++ still has long way to go

---
+++ 5SteamSessions_CTest/Plugins/AdvancedSessions/AdvancedSteamSessions/Intermediate/Build/Win64/UnrealEditor/Inc/AdvancedSteamSessions/UHT/SteamNotificationsSubsystem.generated.h
@@ -0,0 +1,109 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "SteamNotificationsSubsystem.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ADVANCEDSTEAMSESSIONS_SteamNotificationsSubsystem_generated_h
+#error "SteamNotificationsSubsystem.generated.h already included, missing '#pragma once' in SteamNotificationsSubsystem.h"
+#endif
+#define ADVANCEDSTEAMSESSIONS_SteamNotificationsSubsystem_generated_h
+
+#define FID_5SteamSessions_CTest_Plugins_AdvancedSessions_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_16_DELEGATE \
+struct _Script_AdvancedSteamSessions_eventOnSteamOverlayActivated_Parms \
+{ \
+	bool bOverlayState; \
+}; \
+static inline void FOnSteamOverlayActivated_DelegateWrapper(const FMulticastScriptDelegate& OnSteamOverlayActivated, bool bOverlayState) \
+{ \
+	_Script_AdvancedSteamSessions_eventOnSteamOverlayActivated_Parms Parms; \
+	Parms.bOverlayState=bOverlayState ? true : false; \
+	OnSteamOverlayActivated.ProcessMulticastDelegate<UObject>(&Parms); \
+}
+
+
+#define FID_5SteamSessions_CTest_Plugins_AdvancedSessions_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_21_SPARSE_DATA
+#define FID_5SteamSessions_CTest_Plugins_AdvancedSessions_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_21_RPC_WRAPPERS
+#define FID_5SteamSessions_CTest_Plugins_AdvancedSessions_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_21_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_5SteamSessions_CTest_Plugins_AdvancedSessions_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_21_ACCESSORS
+#define FID_5SteamSessions_CTest_Plugins_AdvancedSessions_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_21_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUSteamNotificationsSubsystem(); \
+	friend struct Z_Construct_UClass_USteamNotificationsSubsystem_Statics; \
+public: \
+	DECLARE_CLASS(USteamNotificationsSubsystem, UGameInstanceSubsystem, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/AdvancedSteamSessions"), NO_API) \
+	DECLARE_SERIALIZER(USteamNotificationsSubsystem)
+
+
+#define FID_5SteamSessions_CTest_Plugins_AdvancedSessions_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_21_INCLASS \
+private: \
+	static void StaticRegisterNativesUSteamNotificationsSubsystem(); \
+	friend struct Z_Construct_UClass_USteamNotificationsSubsystem_Statics; \
+public: \
+	DECLARE_CLASS(USteamNotificationsSubsystem, UGameInstanceSubsystem, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/AdvancedSteamSessions"), NO_API) \
+	DECLARE_SERIALIZER(USteamNotificationsSubsystem)
+
+
+#define FID_5SteamSessions_CTest_Plugins_AdvancedSessions_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_21_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API USteamNotificationsSubsystem(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(USteamNotificationsSubsystem) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, USteamNotificationsSubsystem); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(USteamNotificationsSubsystem); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API USteamNotificationsSubsystem(USteamNotificationsSubsystem&&); \
+	NO_API USteamNotificationsSubsystem(const USteamNotificationsSubsystem&); \
+public: \
+	NO_API virtual ~USteamNotificationsSubsystem();
+
+
+#define FID_5SteamSessions_CTest_Plugins_AdvancedSessions_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_21_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API USteamNotificationsSubsystem(USteamNotificationsSubsystem&&); \
+	NO_API USteamNotificationsSubsystem(const USteamNotificationsSubsystem&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, USteamNotificationsSubsystem); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(USteamNotificationsSubsystem); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(USteamNotificationsSubsystem) \
+	NO_API virtual ~USteamNotificationsSubsystem();
+
+
+#define FID_5SteamSessions_CTest_Plugins_AdvancedSessions_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_18_PROLOG
+#define FID_5SteamSessions_CTest_Plugins_AdvancedSessions_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_21_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_5SteamSessions_CTest_Plugins_AdvancedSessions_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_21_SPARSE_DATA \
+	FID_5SteamSessions_CTest_Plugins_AdvancedSessions_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_21_RPC_WRAPPERS \
+	FID_5SteamSessions_CTest_Plugins_AdvancedSessions_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_21_ACCESSORS \
+	FID_5SteamSessions_CTest_Plugins_AdvancedSessions_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_21_INCLASS \
+	FID_5SteamSessions_CTest_Plugins_AdvancedSessions_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_21_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_5SteamSessions_CTest_Plugins_AdvancedSessions_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_21_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_5SteamSessions_CTest_Plugins_AdvancedSessions_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_21_SPARSE_DATA \
+	FID_5SteamSessions_CTest_Plugins_AdvancedSessions_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_21_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_5SteamSessions_CTest_Plugins_AdvancedSessions_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_21_ACCESSORS \
+	FID_5SteamSessions_CTest_Plugins_AdvancedSessions_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_21_INCLASS_NO_PURE_DECLS \
+	FID_5SteamSessions_CTest_Plugins_AdvancedSessions_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_21_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ADVANCEDSTEAMSESSIONS_API UClass* StaticClass<class USteamNotificationsSubsystem>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_5SteamSessions_CTest_Plugins_AdvancedSessions_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ 5SteamSessions_CTest/Source/SteamSessions/C_TopDownController.cpp
@@ -0,0 +1,118 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "C_TopDownController.h"
+#include "GameFramework/Pawn.h"
+#include "Blueprint/AIBlueprintHelperLibrary.h"
+#include "NiagaraSystem.h"
+#include "NiagaraFunctionLibrary.h"
+#include "C_TopDownCharacter.h"
+#include "Engine/World.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+AC_TopDownController::AC_TopDownController()
+{
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+	CachedDestination = FVector::ZeroVector;
+	FollowTime = 0.f;
+}
+
+void AC_TopDownController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//Add Input Mapping Context
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		Subsystem->AddMappingContext(DefaultMappingContext, 0);
+	}
+}
+
+
+void AC_TopDownController::SetupInputComponent()
+{
+	// set up gameplay key bindings
+	Super::SetupInputComponent();
+
+	// Set up action bindings
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent))
+	{
+		// Setup mouse input events
+		EnhancedInputComponent->BindAction(SetDestinationClickAction, ETriggerEvent::Started, this, &AC_TopDownController::OnInputStarted);
+		EnhancedInputComponent->BindAction(SetDestinationClickAction, ETriggerEvent::Triggered, this, &AC_TopDownController::OnSetDestinationTriggered);
+		EnhancedInputComponent->BindAction(SetDestinationClickAction, ETriggerEvent::Completed, this, &AC_TopDownController::OnSetDestinationReleased);
+		EnhancedInputComponent->BindAction(SetDestinationClickAction, ETriggerEvent::Canceled, this, &AC_TopDownController::OnSetDestinationReleased);
+
+		// Setup touch input events
+		EnhancedInputComponent->BindAction(SetDestinationTouchAction, ETriggerEvent::Started, this, &AC_TopDownController::OnInputStarted);
+		EnhancedInputComponent->BindAction(SetDestinationTouchAction, ETriggerEvent::Triggered, this, &AC_TopDownController::OnTouchTriggered);
+		EnhancedInputComponent->BindAction(SetDestinationTouchAction, ETriggerEvent::Completed, this, &AC_TopDownController::OnTouchReleased);
+		EnhancedInputComponent->BindAction(SetDestinationTouchAction, ETriggerEvent::Canceled, this, &AC_TopDownController::OnTouchReleased);
+	}
+}
+
+
+void AC_TopDownController::OnInputStarted()
+{
+	StopMovement();
+}
+
+void AC_TopDownController::OnSetDestinationTriggered()
+{
+	// We flag that the input is being pressed
+	FollowTime += GetWorld()->GetDeltaSeconds();
+
+	// We look for the location in the world where the player has pressed the input
+	FHitResult Hit;
+	bool bHitSuccessful = false;
+	if (bIsTouch)
+	{
+		bHitSuccessful = GetHitResultUnderFinger(ETouchIndex::Touch1, ECollisionChannel::ECC_Visibility, true, Hit);
+	}
+	else
+	{
+		bHitSuccessful = GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility, true, Hit);
+	}
+
+	// If we hit a surface, cache the location
+	if (bHitSuccessful)
+	{
+		CachedDestination = Hit.Location;
+	}
+
+	// Move towards mouse pointer or touch
+	APawn* ControlledPawn = GetPawn();
+	if (ControlledPawn != nullptr)
+	{
+		FVector WorldDirection = (CachedDestination - ControlledPawn->GetActorLocation()).GetSafeNormal();
+		ControlledPawn->AddMovementInput(WorldDirection, 1.0, false);
+	}
+}
+
+void AC_TopDownController::OnSetDestinationReleased()
+{
+	// If it was a short press
+	if (FollowTime <= ShortPressThreshold)
+	{
+		// We move there and spawn some particles
+		UAIBlueprintHelperLibrary::SimpleMoveToLocation(this, CachedDestination);
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, FXCursor, CachedDestination, FRotator::ZeroRotator, FVector(1.f, 1.f, 1.f), true, true, ENCPoolMethod::None, true);
+	}
+
+	FollowTime = 0.f;
+
+}
+
+void AC_TopDownController::OnTouchTriggered()
+{
+	bIsTouch = true;
+	OnSetDestinationTriggered();
+}
+
+void AC_TopDownController::OnTouchReleased()
+{
+	bIsTouch = false;
+	OnSetDestinationReleased();
+}
